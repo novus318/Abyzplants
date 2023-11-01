fc3bd2cf53314e48cc4595e69e9a2f3dba93b116
novus318 <nizamudheen318@gmail.com>
2023-11-01 10:48:15 +0530

    seo

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 export default function Document() {
   return (
     <Html lang="en">
@@ -11,8 +12,30 @@ export default function Document() {
           href="/title.webp"
         />
         <meta name="theme-color" content="#ffffff" />
-        <meta name="description" content="Abyz Plants, the definitive online destination for exceptional indoor and outdoor plants, and a delightful selection of home accessories. Whether it's for offices, malls, hotels, or any setting, count on us for the freshest, healthiest plants. Explore our 'Gift a Plant' option and revel in our swift and reliable delivery service."></meta>
+        <meta name="description" content="Abyz Plants, the definitive online destination for exceptional indoor and outdoor plants, and a delightful selection of home accessories. Whether it's for offices, malls, hotels, or any setting, count on us for the freshest, healthiest plants. Explore our 'Gift a Plant' option and revel in our swift and reliable delivery service."/>
+        <meta name="keywords" content="Abyzplants,Abyzplants UAE,abyzplants uae,Abyzplants dubai,abyzplants dubai,abyzplants,Buy indoor plants online,Buy outdoor plants online,where to buy indoor plants,where to buy outdoor plants,buy indoor plants in Dubai,buy indoor plants in Abu Dhabi,plant stores near me, what are the best indoor plants to buy,indoor plants for my home, flowering indoor plants for home,flowering indoor plants for my office,where can I buy indoor plants for my home,nearest online plant store in Dubai,indoor plant stores near me,online indoor plants,which are the best indoor plants to buy in winter,which are the best indoor plants to buy in summer,outdoor plants in Dubai,where to buy outdoor plants in Dubai,where to buy outdoor plants online,buy outdoor plants online,buy seeds online,buy soil & fertilizers online,buy indoor fertilizers online,buy potting soil online,buy soil for my home,buy plant insecticides,buy plant pesticides,where to buy plant food,where to buy indoor plant pots,where to buy plant pots, where to buy airplants,where to buy large indoor plants, how to water my plants,where to by plant care accessories,indoor plants online,outdoor plants online,flowering plants online,plants gifts online,plant pots online,buy plant pots in Dubai,buy tall indoor plants online,buy tall tree online,buy fertilizers online"/>
+
+        <meta property="og:title" content="Abyzpalnts"/>
+    <meta property="og:description" content="Abyz Plants, the definitive online destination for exceptional indoor and outdoor plants, and a delightful selection of home accessories. Whether it's for offices, malls, hotels, or any setting, count on us for the freshest, healthiest plants. Explore our 'Gift a Plant' option and revel in our swift and reliable delivery service."/>
+    <meta property="og:url" content="https://abyzplants.com/"/>
+    <meta property="og:image" content="https://abyzplants.com/title.webp"/>
+    <meta property="og:type" content="website"/>
+
+    <meta name="twitter:title" content="Abyzpalnts"/>
+    <meta name="twitter:description" content="Abyz Plants, the definitive online destination for exceptional indoor and outdoor plants, and a delightful selection of home accessories. Whether it's for offices, malls, hotels, or any setting, count on us for the freshest, healthiest plants. Explore our 'Gift a Plant' option and revel in our swift and reliable delivery service."/>
+    <meta name="twitter:image" content="https://abyzplants.com/title.webp"/>
+    <meta name="twitter:url" content="https://abyzplants.com/"/>
       <body>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-EEHLS0P5RF" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-EEHLS0P5RF');
+        `}
+      </Script>
         <Main />
         <NextScript />
       </body>
--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -9,6 +9,7 @@ import { useAuth } from '@/store/authContext';
 import { checkUser } from '@/components/checkUser';
 import toast from 'react-hot-toast';
 import InputMask from 'react-input-mask';
+import Layout from '@/components/Layout';
 
 
 const AuthPage = () => {
@@ -84,6 +85,13 @@ const AuthPage = () => {
   });
 
   return (
+    <Layout title='Abyzplants - Join us'
+    description=
+      'Abyz Plants, the definitive online destination for exceptional indoor and outdoor plants in the UAE, with quick delivery options. Our online plant store guarantees the quality of every plant, making it effortless to purchase for your home, office. With the widest variety of options available,and a delightful selection of home accessories. Whether it is for offices, malls, hotels, or any setting, count on us for the freshest, healthiest plants. Explore our (Gift a Plant) option and revel in our swift and reliable delivery service.'
+    keywords=
+      'Abyzplants,Abyzplants UAE,abyzplants uae,Abyzplants dubai,abyzplants dubai,abyzplants,Buy indoor plants online,Buy outdoor plants online,where to buy indoor plants,where to buy outdoor plants,buy indoor plants in Dubai,buy indoor plants in Abu Dhabi,plant stores near me, what are the best indoor plants to buy,indoor plants for my home, flowering indoor plants for home,flowering indoor plants for my office,where can I buy indoor plants for my home,nearest online plant store in Dubai,indoor plant stores near me,online indoor plants,which are the best indoor plants to buy in winter,which are the best indoor plants to buy in summer,outdoor plants in Dubai,where to buy outdoor plants in Dubai,where to buy outdoor plants online,buy outdoor plants online,buy seeds online,buy soil & fertilizers online,buy indoor fertilizers online,buy potting soil online,buy soil for my home,buy plant insecticides,buy plant pesticides,where to buy plant food,where to buy indoor plant pots,where to buy plant pots, where to buy airplants,where to buy large indoor plants, how to water my plants,where to by plant care accessories,indoor plants online,outdoor plants online,flowering plants online,plants gifts online,plant pots online,buy plant pots in Dubai,buy tall indoor plants online,buy tall tree online,buy fertilizers online'
+    author= 'Muhammed Nizamudheen M'
+    canonicalUrl= 'https://abyzplants.com/login' >
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
       <Link
         href="/"
@@ -232,6 +240,7 @@ const AuthPage = () => {
         </div>
       </div>
     </div>
+    </Layout>
   );
 };
 
